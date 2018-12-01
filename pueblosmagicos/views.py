c70e6b1fc5e45b3c2ff17eedad3ed2e39ab5cfb3
Jhoanna <jhoanna.upiita@gmail.com>
2018-11-30 23:27:44 -0800

    Mi aplicacion Django ,primer commit

---
+++ pueblosmagicos/views.py
@@ -0,0 +1,18 @@
+from django.http import Http404
+from django.shortcuts import render
+
+# Create your views here.
+
+from .models import Question
+ 
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'pueblosmagicos/index.html', context)
+
+def detail(request, question_id):
+    try:
+        question = Question.objects.get(pk=question_id)
+    except Question.DoesNotExist:
+        raise Http404("Question does not exist")
+    return render(request, 'pueblosmagicos/detail.html', {'question': question})
